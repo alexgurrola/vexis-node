ad869f8c90040b18dddb0df438c0f37b6cba36d7
Alex Gurrola <alex.gurrola@versidyne.com>
2015-04-22 22:16:41 -0700

    Updated Package & Socket Example

--- sock.js
+++ sock.js
@@ -1,16 +1,14 @@
 var net = require('net');
 
-var client = net.connect({port: 81},
-    function() { //'connect' listener
-        console.log('connected to server!');
-        client.write('world!\r\n');
+var server = net.createServer(function (c) { //'connection' listener
+    console.log('client connected');
+    c.on('end', function () {
+        console.log('client disconnected');
     });
-
-client.on('data', function(data) {
-    console.log(data.toString());
-    client.end();
+    c.write('hello\r\n');
+    c.pipe(c);
 });
 
-client.on('end', function() {
-    console.log('disconnected from server');
+server.listen(81, function () { //'listening' listener
+    console.log('server bound');
 });
