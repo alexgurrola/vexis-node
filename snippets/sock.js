689b298e384a6f48d289bfcc956b8ce39f9a1ba6
alexgurrola <alexgurrola1@gmail.com>
2015-10-20 05:35:57 +0000

    Remove Deprecated Code

--- snippets/sock.js
+++ snippets/sock.js
@@ -2,15 +2,15 @@ var net = require('net');
 
 var server = net.createServer(function (socket) {
 	console.log('Client Connected!');
-	socket.on('end', function () {
+	/*socket.on('end', function () {
         console.log('Client Disconnected!');
     });
     socket.write('Hello!\r\n');
-    socket.pipe(socket);
-	//socket.end("Goodbye!\r\n");
+    socket.pipe(socket);*/
+	socket.end("Goodbye!\r\n");
 });
 
-server.listen({ port: 7900 }, function() {
+server.listen({ port: 7900, exclusive: true }, function() {
 	address = server.address();
 	console.log("Listening: %j", address);
 });
